37a13ee4f6976c151837649278857c2b108f5d9b
Mengkheang Thor <thormengkheang@gmail.com>
2025-01-24 00:10:28 +0700

    initial commit

---
+++ src/day1/BinarySearchList.ts
@@ -0,0 +1,17 @@
+export default function bs_list(haystack: number[], needle: number): boolean {
+    let lo = 0;
+    let hi = haystack.length;
+
+    do {
+        const m = Math.floor(lo + (hi - lo) / 2);
+        const v = haystack[m];
+        if (v === needle) {
+            return true;
+        } else if (needle > v) {
+            lo = m + 1;
+        } else {
+            hi = m;
+        }
+    } while (lo < hi);
+    return false;
+}
