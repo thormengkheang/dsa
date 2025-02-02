1916dfe74ed51495d71a9e3e1e114ccff02eb460
thormengkheang <thormengkheang@gmail.com>
2025-02-02 11:29:27 +0700

    add algo

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,46 @@
+type Node<T> = {
+    value: T;
+    next?: Node<T>;
+};
+
 export default class Queue<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
+    private tail?: Node<T>;
 
     constructor() {
+        this.head = this.tail = undefined;
+        this.length = 0;
     }
 
     enqueue(item: T): void {
+        const node = { value: item };
+        this.length++;
 
-}
+        if (!this.tail) {
+            return;
+        }
+
+        if (this.length === 0) {
+            this.head = this.tail = node;
+            return;
+        }
+
+        this.tail.next = node;
+        this.tail = node;
+    }
+
     deque(): T | undefined {
+        if (!this.head) {
+            return undefined;
+        }
+        this.length--;
+        const head = this.head;
+        this.head = this.head.next;
+        return head.value;
+    }
 
-}
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
