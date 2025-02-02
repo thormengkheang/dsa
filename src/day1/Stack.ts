2e6a102ebf59bba48eae00661c5eb3706ae30154
thormengkheang <thormengkheang@gmail.com>
2025-02-02 11:26:37 +0700

    add more alogrithms

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,18 @@
+type Node<T> = {
+    value: T;
+    prev?: Node<T>;
+};
+
 export default class Stack<T> {
     public length: number;
-
-    
+    private head?: T;
 
     constructor() {
+        this.head = undefined;
+        this.length = 0;
     }
 
-    push(item: T): void {
-
-}
-    pop(): T | undefined {
-
-}
-    peek(): T | undefined {
-
-}
+    push(item: T): void {}
+    pop(): T | undefined {}
+    peek(): T | undefined {}
 }
--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,17 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    const jumpAmt = Math.floor(Math.sqrt(breaks.length));
+    let i = jumpAmt;
+    for (; i < breaks.length; i += jumpAmt) {
+        if (breaks[i]) {
+            break;
+        }
+    }
+    i -= jumpAmt;
+    for (let j = 0; j <= jumpAmt && i < breaks.length; ++j, ++i) {
+        if (breaks[i]) {
+            return i;
+        }
+    }
 
+    return -1;
 }
