6b2669248b37db50ac8b0db5bf210365816e7114
thormengkheang <thormengkheang@gmail.com>
2025-02-03 14:43:02 +0700

    add stack implementation

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -5,14 +5,36 @@ type Node<T> = {
 
 export default class Stack<T> {
     public length: number;
-    private head?: T;
+    private head?: Node<T>;
 
     constructor() {
         this.head = undefined;
         this.length = 0;
     }
 
-    push(item: T): void {}
-    pop(): T | undefined {}
-    peek(): T | undefined {}
+    push(item: T): void {
+        const node = { value: item } as Node<T>;
+        this.length++;
+        if (!this.head) {
+            this.head = node;
+        }
+        node.prev = this.head;
+        this.head = node;
+    }
+
+    pop(): T | undefined {
+        this.length = Math.max(0, this.length - 1);
+        if (this.length === 0) {
+            const head = this.head;
+            this.head = undefined;
+            return head?.value;
+        }
+        const head = this.head;
+        this.head = head?.prev;
+        return head?.value;
+    }
+
+    peek(): T | undefined {
+        return this.head?.value;
+    }
 }
